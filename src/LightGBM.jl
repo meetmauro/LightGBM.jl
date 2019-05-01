2023a05518393ab64e3e2316e99f953a5eb12243
Mauro Lombarda <meetmauro@gmail.com>
2019-05-01 10:58:59 +0200

    Fixing library resolution at startup

--- src/LightGBM.jl
+++ src/LightGBM.jl
@@ -4,33 +4,23 @@ module LightGBM
 
 using Libdl
 
-# Macro to load a library
-macro checked_lib(libname, path)
-    if Libdl.dlopen_e(path) == C_NULL
-        error("Unable to load \n\n$libname ($path)\n\nPlease re-run Pkg.build(package), and restart Julia.")
-    end
-    quote
-        const $(esc(libname)) = $path
-    end
+const LGBM_library = find_library(["lib_lightgbm.so", "lib_lightgbm.dll", "lib_lightgbm.dylib"], [ENV["LIGHTGBM_PATH"]])
+
+function __init__()
+   global LGBM_library
+   if !haskey(ENV, "LIGHTGBM_PATH")
+      error("Environment variable LIGHTGBM_PATH not found. ",
+         "Set this variable to point to the LightGBM directory prior to loading LightGBM.jl ",
+         "(e.g. `ENV[\"LIGHTGBM_PATH\"] = \"../LightGBM\"`).")
+   else
+      slib = find_library(["lib_lightgbm.so", "lib_lightgbm.dll", "lib_lightgbm.dylib"], [ENV["LIGHTGBM_PATH"]])
+      if slib == ""
+         error("Could not open the LightGBM library at $(ENV["LIGHTGBM_PATH"]). ",
+            "Set this variable to point to the LightGBM directory prior to loading LightGBM.jl ",
+            "(e.g. `ENV[\"LIGHTGBM_PATH\"] = \"../LightGBM\"`).")
+      end
+   end
 end
-
-@checked_lib LGBM_library "/usr/local/lib/lib_lightgbm.so"
-
-# function __init__()
-#    global LGBM_library
-#    if !haskey(ENV, "LIGHTGBM_PATH")
-#       error("Environment variable LIGHTGBM_PATH not found. ",
-#          "Set this variable to point to the LightGBM directory prior to loading LightGBM.jl ",
-#          "(e.g. `ENV[\"LIGHTGBM_PATH\"] = \"../LightGBM\"`).")
-#    else
-#       LGBM_library = find_library(["lib_lightgbm.so", "lib_lightgbm.dll", "lib_lightgbm.dylib"], [ENV["LIGHTGBM_PATH"]])
-#       if LGBM_library == ""
-#          error("Could not open the LightGBM library at $(ENV["LIGHTGBM_PATH"]). ",
-#             "Set this variable to point to the LightGBM directory prior to loading LightGBM.jl ",
-#             "(e.g. `ENV[\"LIGHTGBM_PATH\"] = \"../LightGBM\"`).")
-#       end
-#    end
-# end
 
 using Compat
 
